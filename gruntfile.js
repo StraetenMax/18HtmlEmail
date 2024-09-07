5aa0bddf2a42b9d10d9e6f13a080c282ffa11d9d
philippe <maxstraeten@yahoo.com>
2024-09-07 13:18:33 +0200

    installation de grunt

---
+++ gruntfile.js
@@ -0,0 +1,14 @@
+
+
+
+grunt.initConfig({
+    inlinecss: {
+        main: {
+            options: {
+            },
+            files: {
+                'out.html': 'in.html'
+            }
+        }
+    }
+})
